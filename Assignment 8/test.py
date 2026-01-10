3ee0e6d92ee784fcfdc857059a659af57c874bbc
Abdul Faiq <abdulfaiq47@gmail.com>
2026-01-10 13:13:47 +0500

    Add files via upload

---
+++ Assignment 8/test.py
@@ -0,0 +1,11 @@
+import numpy
+import pandas
+import sklearn
+import matplotlib
+import seaborn
+
+print("numpy:", numpy.__version__)
+print("pandas:", pandas.__version__)
+print("sklearn:", sklearn.__version__)
+print("matplotlib:", matplotlib.__version__)
+print("seaborn:", seaborn.__version__)
